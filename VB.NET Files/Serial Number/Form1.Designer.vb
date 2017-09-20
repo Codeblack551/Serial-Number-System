a603b84474cc5890ac3d3f786a83070543475e61
ThaisenPM <21050437+ThaisenPM@users.noreply.github.com>
2017-09-19 18:56:09 -0600

    Initial Push

---
+++ VB.NET Files/Serial Number/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Serial number to check: "
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 51)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(260, 172)
+        Me.WebBrowser1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(197, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 20)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(61, 229)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(211, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 232)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "HWID: "
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Serial Number Check"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ VB.NET Files/Serial Number/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Threading
+Imports System.Management
+Imports System.Security
+
+Public Class Form1
+
+    Dim hwid As String
+    Dim check As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        check = 0
+
+        Dim hw As New clsComputerInfo
+        Dim cpu As String
+        Dim mb As String
+        Dim mac As String
+        Dim hwid As String
+        cpu = hw.GetProcessorId()
+        mb = hw.GetMotherboardID()
+        mac = hw.GetMACAddress()
+        hwid = cpu + mb + mac
+        Dim hwidEncrypted As String = Strings.UCase(hw.getMD5Hash(cpu & mb & mac))
+        hwid = hwidEncrypted
+        TextBox2.Text = hwid
+
+        If (My.Settings.Installed = True) Then
+            TextBox1.Text = My.Settings.Serial
+            Timer1.Interval = 1
+            Timer1.Start()
+            check = 1
+        End If
+    End Sub
+
+    Private Class clsComputerInfo
+        'Get processor ID
+        Friend Function GetProcessorId() As String
+            Dim strProcessorID As String = String.Empty
+            Dim query As New SelectQuery("Win32_processor")
+            Dim search As New ManagementObjectSearcher(query)
+            Dim info As ManagementObject
+            For Each info In search.Get()
+                strProcessorID = info("processorID").ToString()
+            Next
+            Return strProcessorID
+        End Function
+        ' Get MAC Address
+        Friend Function GetMACAddress() As String
+            Dim mc As ManagementClass = New ManagementClass("Win32_NetworkAdapterConfiguration")
+            Dim moc As ManagementObjectCollection = mc.GetInstances()
+            Dim MacAddress As String = String.Empty
+            For Each mo As ManagementObject In moc
+                If (MacAddress.Equals(String.Empty)) Then
+                    If CBool(mo("IPEnabled")) Then MacAddress = mo("MacAddress").ToString()
+                    mo.Dispose()
+                End If
+                MacAddress = MacAddress.Replace(":", String.Empty)
+            Next
+            Return MacAddress
+        End Function
+        ' Get Motherboard ID
+        Friend Function GetMotherboardID() As String
+            Dim strMotherboardID As String = String.Empty
+            Dim query As New SelectQuery("Win32_BaseBoard")
+            Dim search As New ManagementObjectSearcher(query)
+            Dim info As ManagementObject
+            For Each info In search.Get()
+                strMotherboardID = info("product").ToString()
+            Next
+            Return strMotherboardID
+        End Function
+        ' Encrypt HWID
+        Friend Function getMD5Hash(ByVal strToHash As String) As String
+            Dim md5Obj As New System.Security.Cryptography.MD5CryptoServiceProvider
+            Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+            bytesToHash = md5Obj.ComputeHash(bytesToHash)
+            Dim strResult As String = ""
+            For Each b As Byte In bytesToHash
+                strResult += b.ToString("x2")
+            Next
+            Return strResult
+        End Function
+    End Class
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        check = 1
+        Button1.Enabled = False
+        WebBrowser1.Navigate("http://localhost/serial/serial_get.php?serial=" + TextBox1.Text + "&hwidin=" + TextBox2.Text + "&submit=Submit")
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        If (check = 1) Then
+            If (WebBrowser1.DocumentText.Contains("0")) Then
+                check = 0
+                Button1.Enabled = True
+                Timer1.Stop()
+                MsgBox("Wrong HWID")
+            ElseIf (WebBrowser1.DocumentText.Contains("1")) Then
+                check = 0
+                Button1.Enabled = True
+                My.Settings.Installed = True
+                My.Settings.Serial = TextBox1.Text
+                My.Settings.Save()
+                Timer1.Stop()
+                MsgBox("All info correct!")
+            ElseIf (WebBrowser1.DocumentText.Contains("2")) Then
+                check = 0
+                Button1.Enabled = True
+                Timer1.Stop()
+                MsgBox("HWID field left empty")
+            ElseIf (WebBrowser1.DocumentText.Contains("3")) Then
+                check = 0
+                Button1.Enabled = True
+                Timer1.Stop()
+                MsgBox("No serial with that key")
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        WebBrowser1.Navigate("http://localhost/serial/serial_get.php?serial=" + TextBox1.Text + "&hwidin=" + TextBox2.Text + "&submit=Submit")
+    End Sub
+End Class
